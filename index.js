46361fe2c648b610b6287e20d03180d9ff8b6db5
Karen V <karenvo@usc.edu>
2019-12-27 10:14:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet() {
+  if (time < 12){
+    console.log("Good Morning")
+  } else if (time =< 17) {
+    console.log("Good Afternoon")
+  } else if (time > 17) {
+    console.log("Good Evening")
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage() {
+  
+}
